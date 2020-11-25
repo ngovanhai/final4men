2305a2544709dcc54b9e5ba6e631857f0817da7e
ngô văn  hải <55077450+ngovanhai@users.noreply.github.com>
2020-11-25 10:30:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const path = require('path')
 
 const app = express();
 app.use(express.json());
